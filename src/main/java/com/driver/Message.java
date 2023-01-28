8d4e26f45c94473c92a8fccc218ae8b7f6092c56
Rahul14121999 <rahulsshrivastava09@gmail.com>
2023-01-29 00:26:45 +0530

    solution 3

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,15 +6,22 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
-    public Message()
-    {
 
+
+    public Message(int id) {
+        this.id = id;
+        this.timestamp = new Date();
     }
 
-    public Message(int id, String content, Date timestamp) {
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = new Date();
+    }
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 
     public int getId() {
